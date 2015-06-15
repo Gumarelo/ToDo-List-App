a565b78faf7dec276aa4423f8e779e130f5b735b
Jorge Casariego <jorgekasa@gmail.com>
2015-06-15 18:15:10 -0400

    Initial Commit

---
+++ ToDo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToDo
+//
+//  Created by Jorge Casariego on 15/6/15.
+//  Copyright (c) 2015 Jorge Casariego. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
